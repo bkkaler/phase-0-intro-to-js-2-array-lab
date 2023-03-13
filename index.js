dd8a5e6ff91153ae7ae9b05594dd88e5992e9969
Baldip Kaler <kalerbk19@gmail.com>
2023-03-12 19:34:25 -0700

    lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return[...cats,name];
+}
+
+function prependCat(name) {
+    const cats = ["Milo", "Otis", "Garfield"]
+    return[name, ...cats]
+}
+const newcats = [... cats]
+function removeLastCat(name) {
+    return newcats.slice(0,cats.length-1)
+}
+function removeFirstCat(name) {
+    return newcats.slice(1)
+}
